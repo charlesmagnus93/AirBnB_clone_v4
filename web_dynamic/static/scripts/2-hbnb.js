b1e65a8fd1eca172b5111e00ec2ee4fda33732fa
Ch@rlem@gne <cagbolossou@dactechnologies.net>
2022-11-29 18:41:48 +0000

    Api status

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -9,4 +9,18 @@ $(document).ready(function() {
     }
     $('.amenities h4').text(amenities.join(', '));
   });
+  apiStatus();
 });
+
+function apiStatus () {
+  $.get('http://0.0.0.0:5001/api/v1/status/',
+  function (data, textStatus) {
+    if (textStatus === 'success') {
+      $('#api_status').addClass('available');
+      console.log('ok');
+    } else {
+      $('#api_status').removeClass('available');
+      console.log('ko');
+    }
+  });
+}
