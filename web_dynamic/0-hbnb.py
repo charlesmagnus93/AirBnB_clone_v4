ca8ac9ddf2570e851d22eca493404fd5f5823ee6
Ch@rlem@gne <cagbolossou@dactechnologies.net>
2022-11-29 15:50:24 +0000

    Cash only

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -25,7 +25,7 @@ def hbnb():
     states = storage.all(State).values()
     states = sorted(states, key=lambda k: k.name)
     st_ct = []
-    cache_id=str(uuid.uuid4())
+    cache_id=uuid.uuid4()
 
     for state in states:
         st_ct.append([state, sorted(state.cities, key=lambda k: k.name)])
@@ -36,7 +36,7 @@ def hbnb():
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
 
-    return render_template('100-hbnb.html',
+    return render_template('0-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
                            places=places,
